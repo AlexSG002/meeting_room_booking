2c69a5a603e2519770a8a9aae3352ff1111b9aed
Alex <alejandrosanchezgil7@gmail.com>
2026-07-22 18:20:41 +0200

    feat: Creadas excepciones y validaciones para comprobar reservas solapadas o inválidas por fecha.

--- src/main/java/com/alejandro/meetingbooking/repository/ReservationRepository.java
+++ src/main/java/com/alejandro/meetingbooking/repository/ReservationRepository.java
@@ -2,8 +2,22 @@ package com.alejandro.meetingbooking.repository;
 
 import com.alejandro.meetingbooking.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalDateTime;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
+    @Query("SELECT COUNT(r) > 0 " +
+            "FROM Reservation r " +
+            "WHERE r.room.id =:roomId " +
+            "AND r.startTime <:endTime " +
+            "AND r.endTime >:startTime")
+    boolean existsOverlappingReservation(
+            @Param("roomId") Long roomId,
+            @Param("startTime") LocalDateTime startTime,
+            @Param("endTime") LocalDateTime endTime
+            );
 }
--- src/main/java/com/alejandro/meetingbooking/service/impl/ReservationServiceImpl.java
+++ src/main/java/com/alejandro/meetingbooking/service/impl/ReservationServiceImpl.java
@@ -2,7 +2,11 @@ package com.alejandro.meetingbooking.service.impl;
 
 import com.alejandro.meetingbooking.dto.request.CreateReservationRequest;
 import com.alejandro.meetingbooking.dto.response.ReservationResponse;
+import com.alejandro.meetingbooking.entity.Employee;
 import com.alejandro.meetingbooking.entity.Reservation;
+import com.alejandro.meetingbooking.entity.Room;
+import com.alejandro.meetingbooking.exception.InvalidReservationException;
+import com.alejandro.meetingbooking.exception.ReservationConflictException;
 import com.alejandro.meetingbooking.exception.ResourceNotFoundException;
 import com.alejandro.meetingbooking.mapper.ReservationMapper;
 import com.alejandro.meetingbooking.repository.EmployeeRepository;
@@ -12,6 +16,8 @@ import com.alejandro.meetingbooking.service.ReservationService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+
 @Service
 @RequiredArgsConstructor
 public class ReservationServiceImpl implements ReservationService {
@@ -20,11 +26,38 @@ public class ReservationServiceImpl implements ReservationService {
     private final RoomRepository roomRepo;
     @Override
     public ReservationResponse createReservation(CreateReservationRequest request) {
+
+        if(!request.getStartTime().isBefore(request.getEndTime())){
+            throw new InvalidReservationException("The start time must be before the end time.");
+        }
+
+        if(!request.getStartTime().isAfter(LocalDateTime.now())){
+            throw new InvalidReservationException("Choose a valid start date");
+        }
+
+        Employee employee = employeeRepo.findById(request.getEmployeeId())
+                .orElseThrow(() -> new ResourceNotFoundException("Employee not found"));
+
+        Room room = roomRepo.findById(request.getRoomId())
+                .orElseThrow(() -> new ResourceNotFoundException("Room not found"));
+
         Reservation reservation = ReservationMapper.toEntity(request);
-        reservation.setEmployee(employeeRepo.findById(request.getEmployeeId()).orElseThrow(() -> new ResourceNotFoundException("Employee not found")));
-        reservation.setRoom(roomRepo.findById(request.getRoomId()).orElseThrow(() -> new ResourceNotFoundException("Room not found")));
+        reservation.setEmployee(employee);
+        reservation.setRoom(room);
+
+        boolean overlapping = reservationRepo.existsOverlappingReservation(
+                request.getRoomId(),
+                request.getStartTime(),
+                request.getEndTime()
+        );
+
+        if(overlapping) {
+            throw new ReservationConflictException("The room is already reserved for the selected time slot");
+        }
+
         Reservation savedReservation = reservationRepo.save(reservation);
 
         return ReservationMapper.toResponse(savedReservation);
     }
+
 }
