7fb51146e75cbde477887f829565e94e76e011d3
Alex <alejandrosanchezgil7@gmail.com>
2026-07-22 17:13:36 +0200

    fix: Añadida anotación de repository

--- src/main/java/com/alejandro/meetingbooking/repository/ReservationRepository.java
+++ src/main/java/com/alejandro/meetingbooking/repository/ReservationRepository.java
@@ -2,6 +2,8 @@ package com.alejandro.meetingbooking.repository;
 
 import com.alejandro.meetingbooking.entity.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Long> {
 }
